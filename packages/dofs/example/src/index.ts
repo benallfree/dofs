62ea5cfefd05051e6de8a7f4510ca027989b332b
Ben Allfree <ben@benallfree.com>
2025-06-09 17:57:35 -0700

    enhance test suite

--- packages/dofs/example/src/index.ts
+++ packages/dofs/example/src/index.ts
@@ -3,25 +3,37 @@ import { withDofs } from 'dofs'
 import { dofs } from 'dofs/hono'
 import { Hono } from 'hono'
 
-export class MyDurableObject extends withDofs(DurableObject<Env>, { chunkSize: 4 * 1024 }) {}
+export class MyDurableObject1 extends withDofs(DurableObject<Env>, { chunkSize: 4 * 1024 }) {}
+export class MyDurableObject2 extends withDofs(DurableObject<Env>, { chunkSize: 4 * 1024 }) {}
 
 const app = new Hono<{ Bindings: Env }>()
 
 // Mount the API middleware
 app.route(
   '/',
-  dofs({
+  dofs<Env>({
     MY_DURABLE_OBJECT: {
-      classRef: MyDurableObject,
+      classRef: MyDurableObject1,
       getInstances: async () => {
         return [
           {
-            slug: 'my-durable-object',
-            name: 'My Durable Object',
+            slug: 'instance-1',
+            name: 'Instance 1',
+          },
+          {
+            slug: 'instance-2',
+            name: 'Instance 2',
           },
         ]
       },
       name: 'My Durable Object',
+    },
+    MY_DURABLE_OBJECT_1: {
+      classRef: MyDurableObject2,
+      getInstances: async () => {
+        return [{ slug: 'instance-1', name: 'Instance 1' }]
+      },
+      name: 'My Durable Object 1',
     },
   }) as any
 )
