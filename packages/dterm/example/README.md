a4b1d19c2c1fa7cafa1e1d96be0df2798628f5ac
Ben Allfree <ben@benallfree.com>
2025-06-21 21:09:32 -0700

    move dterm to separate repo

--- packages/dterm/example/README.md
+++
@@ -1,3 +0,0 @@
-# DTerm Demo
-
-This is an example of how to use DTerm.
--- packages/dterm/src/vanilla/index.ts
+++
@@ -1,5 +0,0 @@
-import { DTermElement } from './DTermElement'
-export { dterm } from './dterm'
-export type { FSStat, Options, StatEntry } from './dterm'
-
-customElements.define('d-term', DTermElement)
